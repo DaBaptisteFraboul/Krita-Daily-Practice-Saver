103095ee2d453993a6b909498ca1f97d0ef96849
baptiste.fraboul <fraboulbaptiste.pro@gmail.com>
2025-08-10 19:44:17 +0200

    fix: now increment number per day

--- utils.py
+++ utils.py
@@ -8,6 +8,7 @@ import calendar
 ROOT = r"D:\_ART\{current_year}\practice\{month}"
 FILENAME = r"{iso_date}__practice__w{file_number}.kra"
 FILE_NUMBER_PATTERN = r"__w(?P<number>\d{3}).kra"
+DATE_PATTERN = r"^(?P<iso_date>((?P<year>\d{4})_(?P<month>\d{2})_(?P<day>\d{2})))__practice__w(?P<number>\d{3})"
 
 
 def format_current_isodate(today:date) -> str:
@@ -15,19 +16,27 @@ def format_current_isodate(today:date) -> str:
 
 
 def get_latest_available_number(practice_directory:str)-> int:
+    today=date.today()
     if not os.path.exists(practice_directory):
         return 1
     else :
-        sorting = [1]
+        sort_by_day = {} #files are sorted by month, get_only the day
         for filename in os.listdir(practice_directory):
             number_match = re.search(
-                pattern=FILE_NUMBER_PATTERN,
+                pattern=DATE_PATTERN,
                 string=filename
             ) 
-            if number_match : 
-                sorting.append(int(number_match.group("number")))
-               
-        return max(sorting) + 1
+            if number_match :
+                fileday = number_match.group("day")
+                sort_by_day.setdefault(fileday, set())
+                
+                sort_by_day[fileday].add(int(number_match.group("number")))
+        
+        current_day = str(today.day)
+
+        current_number= max(sort_by_day.get(current_day, [0])) 
+
+        return current_number + 1
 
           
 def generate_available_filepath(number_padding:int) -> str :
@@ -53,5 +62,12 @@ def generate_available_filepath(number_padding:int) -> str :
 
         
 if __name__ == "__main__":
-    a = generate_available_filepath()
+    today = date.today()
+    practice_directory = Path(
+        ROOT.format(
+            current_year=today.year,
+            month=calendar.month_name[today.month].lower()
+        )
+    ).as_posix()
+    a = get_latest_available_number(practice_directory=practice_directory)
     print(a)
